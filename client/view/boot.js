5ea88c4e7acc77adb0fac5c525852fac29a45773
Yadd95 <deakdani0216@gmail.com>
2017-03-06 14:13:15 +0100

    Kezdeti főoldal

--- client/view/boot.js
+++ client/view/boot.js
@@ -6,3 +6,27 @@ import * as remoteActions from '../remoteActions'
 export default function () {
   console.log('View boot script kész!')
 }
+
+// Segédfüggvények
+
+function $(selector) {
+    return document.querySelector(selector)
+}
+
+function $$(selector) {
+    return document.querySelectorAll(selector)
+}
+
+//ha megnyomja a startot, felugrik egy ablak, ami megkérdezi a nevét
+$('#startBtn').addEventListener('click', newPlayerInit)
+
+//ha megkapja a nevet, átirányítja a játék oldalára
+function newPlayerInit(){
+  var playerName = prompt("Please enter your name", "Han Solo");
+  if (playerName == '') playerName = 'Névtelenke'
+  if (playerName != null) { //egyelőre csak placeholderként default szöveg
+    $('#welcomePage').style.display = 'none'
+    $('#gameArea').style.display = 'block'
+    $('#gameArea').innerHTML = "Hello " + playerName + "! Let's play some Solo!"
+  }
+}
