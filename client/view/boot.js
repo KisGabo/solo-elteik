f06dcb025adf1db917f524757e6562dcff2ae70f
Yadd95 <deakdani0216@gmail.com>
2017-03-06 18:28:53 +0100

    Connect/Disconnect működések, valamint a várakozó lobby

--- client/view/boot.js
+++ client/view/boot.js
@@ -1,5 +1,4 @@
 import * as remoteActions from '../remoteActions'
-
 /**
  * Ez a függvény egyszer lefut, amint betöltött az oldal.
  */
@@ -20,13 +19,30 @@ function $$(selector) {
 //ha megnyomja a startot, felugrik egy ablak, ami megkérdezi a nevét
 $('#startBtn').addEventListener('click', newPlayerInit)
 
-//ha megkapja a nevet, átirányítja a játék oldalára
+/* ha megkapja a nevet, átirányítja a várakozás oldalára, 
+tovább küldi a connectet a szerver felé */
 function newPlayerInit(){
-  var playerName = prompt("Please enter your name", "Han Solo");
+  var playerName = prompt("Kérlek, add meg a neved!", "Han Solo");
   if (playerName == '') playerName = 'Névtelenke'
   if (playerName != null) { //egyelőre csak placeholderként default szöveg
     $('#welcomePage').style.display = 'none'
-    $('#gameArea').style.display = 'block'
-    $('#gameArea').innerHTML = "Hello " + playerName + "! Let's play some Solo!"
+    $('#waitArea').style.display = 'block'
+    $('#waitArea').innerHTML = "Üdv " + playerName + "! Kérlek várj egy kicsit!" +"<br>" +'<img src="images/view/rolling.svg">'
+    remoteActions.connect(playerName)
+    waitArea()
   }
 }
+//a várakozó "menü" felépítése
+function waitArea(){
+  $('#waitArea').innerHTML += "<br> <h3> Várakozó játékosok: </h3>"
+
+  var players = document.createElement('ul')
+  players.setAttribute('id', 'players')
+
+  $('#waitArea').appendChild(players)
+}
+//bezárás érzékelése
+window.onbeforeunload = function(){
+   remoteActions.disconnect()
+}
+
--- client/view/gameEvents.js
+++ client/view/gameEvents.js
@@ -4,12 +4,26 @@
  * Bővebb infó: /docs/szerver-iface.md "Szerver --> kliens" rész
  */
 
+/* ha jön a jelzés a szervertől, hogy új ember csatlakozott, beszúr egy új 
+elemet a listába */
 export function playerConnected (data) {
+    var newPlayer = document.createElement('li')
+    newPlayer.setAttribute('class','playerInWaiting')
+    newPlayer.setAttribute('id', 'player-'+data.id)
+    newPlayer.innerHTML = 'Sorszám: ' + data.id + ' Név: ' + data.name
+    
+
+    $('#players').appendChild(newPlayer)
 
 }
 
+/* szerveroldali jelzésre kivesz valakit a listából Id alapján */
 export function playerDisconnected (playerId) {
+    if(true){
+        $('#players').removeChild(document.getElementById("player-"+playerId))
+    }else{
 
+    }
 }
 
 export function started (data) {
